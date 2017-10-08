59c2589f405afffe171a15de34bf509d3a793223
avaxxx <avaxxx@gmail.com>
2017-10-08 11:30:50 +0200

    Changes

---
+++ Demo 1/Commands.ps1
@@ -0,0 +1,21 @@
+Install-Module -Scope CurrentUser posh-docker
+
+Import-Module posh-docker
+
+
+Clear-Host
+
+#get the docker information
+docker version
+
+Clear-Host
+
+docker.exe images 
+#region test     
+
+#endregion  
+
+
+
+
+docker.exe container run dockeronwindows/ch01-whale
