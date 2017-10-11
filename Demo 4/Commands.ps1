bafe3cae701d9918bb624dd0559c72d3d35730a2
avaxxx <avaxxx@gmail.com>
2017-10-11 22:07:52 +0200

    Progress

--- Demo 4/Commands.ps1
+++ Demo 4/Commands.ps1
@@ -12,11 +12,40 @@ chcp 852
 
 docker-compose build
 
+docker-compose up
+
+docker-compose top
+
+####################
+
 dotnet publish -o ./bin/PublishOutput
 
 docker-compose -f docker-compose.debug.yml build
 
 docker-compose -f docker-compose.debug.yml up
+
+##############
+
+az group create --name ContainerService --location westeurope
+
+az acs create --name ContainerService --orchestrator-type Swarm --resource-group ContainerServiceGroup --generate-ssh-keys --agent-vm-size Standard_A2_v2
+
+
+az network public-ip list --resource-group ContainerService --query "[*].{Name:name,IPAddress:ipAddress}" -o table
+
+
+#swarm-agent-ip-quadrotechagents-1F6F72ED  52.232.39.223
+#swarm-master-ip-quadrotechmgmt-1F6F72ED   104.45.12.33
+
+#Your public key has been saved in /home/avaxxx/.ssh/id_rsa.pub.
+
+ssh azureuser@104.45.12.33 -A -p 2200 -i <PUB_FILE_LOCATION>
+
+#ssh -p 2200 -fNL 2375:localhost:2375 azureuser@104.45.12.33 -i /mnt/c/Users/avaxx/.ssh/id_rsa.pub
+
+az group delete --name ContainerServiceGroup --yes --no-wait
+
+
 
 docker rmi $(docker images -f "dangling=true" -q)
 
