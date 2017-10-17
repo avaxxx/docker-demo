d9ed87aed91e79af26d8615da9b8fe9d877d2fb3
avaxxx <avaxxx@gmail.com>
2017-10-17 12:15:32 +0200

    Ch

--- Commands.ps1
+++ Commands.ps1
@@ -5,3 +5,18 @@ Import-Module posh-docker
 Clear-Host
 
 docker rmi $(docker images -f "dangling=true" -q)
+
+# webpages with dockerfiles
+
+# https://github.com/kiasaki/docker-alpine-postgres
+
+# https://github.com/dotnet/dotnet-docker
+
+# https://github.com/aspnet/aspnet-docker
+
+# https://portal.azure.com/#blade/HubsExtension/Resources/resourceType/Microsoft.ContainerRegistry%2Fregistries
+
+# https://portal.azure.com/#blade/HubsExtension/Resources/resourceType/Microsoft.ContainerService%2FcontainerServices
+
+# 
+
